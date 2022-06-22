9855b9a2cc89bac569f8df18e13a98fcbc7a7e60
Liam Moat <contact@liammoat.com>
2022-06-22 16:48:42 +0100

    feat(native): initial web with jaeger

---
+++ src/native/OTelInAzure.Native.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+using System.Diagnostics;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+using OTelInAzure.Core;
+
+namespace OTelInAzure.Native.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly Random _random = new Random();
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly IConfiguration _configuration;
+    private readonly ActivitySource _activitySource;
+
+    public WeatherForecastController(IConfiguration configuration, ActivitySource activitySource)
+    {
+        _configuration = configuration;
+        _activitySource = activitySource;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public async Task<WeatherForecast> GetAsync()
+    {
+        using var activity = _activitySource.StartActivity("GetWeatherForecast");
+
+        await new BlobClient(connectionString: _configuration.GetConnectionString("AzureStorage"), blobContainerName: "content", blobName: "hello-world.txt").DownloadContentAsync();
+        activity?.AddEvent(new ActivityEvent("Blob download complete"));
+
+        var random = _random.Next(1, 5);
+        if (random % 2 == 0) await Task.Delay(random * 300);
+        activity?.AddEvent(new ActivityEvent("Random compute complete"));
+
+        activity?.SetStatus(ActivityStatusCode.Ok);
+
+        return new WeatherForecast
+        {
+            Date = DateTime.Now,
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        };
+    }
+}
---
+++ src/native/OTelInAzure.Native.Web/Program.cs
@@ -0,0 +1,40 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var resourceAttributes = new Dictionary<string, object> {
+    { "service.name", "otel-native-web" },
+    { "service.instance.id", Environment.MachineName }};
+var resourceBuilder = ResourceBuilder.CreateDefault().AddAttributes(resourceAttributes);
+
+builder.Services.AddOpenTelemetryTracing(x => x
+    .SetResourceBuilder(resourceBuilder)
+    .AddHttpClientInstrumentation()
+    .AddAspNetCoreInstrumentation()
+    .AddJaegerExporter());
+
+builder.Services.AddHttpClient();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
