3d4a07b53a91c5176d615dfec799fe923847c00f
Liam Moat <contact@liammoat.com>
2022-06-22 17:04:32 +0100

    feat(azure): initial azure web app

---
+++ src/azure/OTelInAzure.Azure.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OTelInAzure.Core;
+
+namespace OTelInAzure.Azure.Web.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<IndexModel> _logger;
+
+    public string WeatherSummary { get; set; }
+
+    public IndexModel(IHttpClientFactory httpClientFactory,
+        IConfiguration configuration, ILogger<IndexModel> logger)
+    {
+        _httpClientFactory = httpClientFactory;
+        _configuration = configuration;
+        _logger = logger;
+    }
+
+    public async Task OnGet()
+    {
+        try
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            httpClient.BaseAddress = new Uri(_configuration["APIBaseAddress"]);
+            var result = await httpClient.GetFromJsonAsync<WeatherForecast>("weatherforecast");
+            WeatherSummary = result.Summary;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unable to retreive weather forecast.");
+        }
+    }
+}
---
+++ src/azure/OTelInAzure.Azure.Web/Program.cs
@@ -0,0 +1,44 @@
+using Azure.Monitor.OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var resourceAttributes = new Dictionary<string, object> {
+    { "service.name", "otel-azure-web" },
+    { "service.instance.id", Environment.MachineName }};
+var resourceBuilder = ResourceBuilder.CreateDefault().AddAttributes(resourceAttributes);
+
+builder.Services.AddOpenTelemetryTracing(x => x
+    .SetResourceBuilder(resourceBuilder)
+    .AddHttpClientInstrumentation()
+    .AddAspNetCoreInstrumentation()
+    .AddAzureMonitorTraceExporter(config =>
+    {
+        config.ConnectionString = "InstrumentationKey=67b15f61-70e1-4964-9ce9-1130fcaa3952;IngestionEndpoint=https://uksouth-1.in.applicationinsights.azure.com/;LiveEndpoint=https://uksouth.livediagnostics.monitor.azure.com/";
+    }));
+
+builder.Services.AddHttpClient();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
